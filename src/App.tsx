8493623e280d3088aa8005e6ee7626dfe9c9d8ef
LLpanov <leonardopanov@yahoo.com>
2022-11-20 06:07:28 +0200

    tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css'
+
+const App = () => {
+    return (
+        <>
+            <h1 className="font-bold text-3xl">hello</h1>
+            <li >ads</li>
+        </>
+    );
+};
+
+export default App;
